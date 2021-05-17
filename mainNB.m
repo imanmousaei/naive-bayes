13b1eec71eba7dbbde8950453c0dbbfeabbd467a
Iman Mousaei <imanmousaei1379@gmail.com>
2021-05-18 00:55:39 +0430

    confusion matrix

--- mainNB.m
+++ mainNB.m
@@ -11,17 +11,21 @@ close all
 %% constants
 classesNo = 2;
 featuresNo = 3;
-testRate = 0;
+testRate = 1/9;
 
 %% trainig data 
-fr(1,:) = [5.92, 5.58, 5.92, 5, 5.5, 5.42, 5.75, 6];
-fr(2,:) = [190, 170, 165, 100, 150, 130, 150, 180];
-fr(3,:) = [11, 12, 10, 6, 8, 7, 9, 12];
-gender = [2, 2, 2, 1, 1, 1, 1, 2]; % 2 = male, 1 = female
+fr(1,:) = [5.92, 5.58, 5.92, 5, 5.5, 5.42, 5.75, 6, 6];
+fr(2,:) = [190, 170, 165, 100, 150, 130, 150, 180, 130];
+fr(3,:) = [11, 12, 10, 6, 8, 7, 9, 12, 8];
+gender = [2, 2, 2, 1, 1, 1, 1, 2, 1]; % 2 = male, 1 = female
+
+%% todo
+% read from excel standard form : 5.92 190 11 2
+% confusion matrix
 
 
 %% seperate genders
-trainingN = (1-testRate)*numel(gender);
+trainingN = round((1-testRate)*numel(gender))
 N = numel(gender)
 features = [[[],[],[]], [[],[],[]]];
 
@@ -47,11 +51,16 @@ for f=1:featuresNo
 end
 
 
-means
-vars
-    
-
 %% test
-gender = classify([6,130,8], classesNo, featuresNo, means, vars)
+gdr = [];
+for i=trainingN+1:N
+    tmp = [];
+    for f=1:featuresNo 
+        tmp(end+1) = fr(f,i);
+    end
+    gdr(i-trainingN) = classify([6,130,8], classesNo, featuresNo, means, vars);
+end
 
+
+plotconfusion(categorical(gender(trainingN+1:end)),categorical(gdr));
 
