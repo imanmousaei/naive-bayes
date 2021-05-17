f43f97f4d9c7b1e942ee3742586e798d4e3ccbd8
Iman Mousaei <imanmousaei1379@gmail.com>
2021-05-17 18:17:51 +0430

    seperate classes data

--- mainNB.m
+++ mainNB.m
@@ -1,7 +1,5 @@
 %% problem : 
-%   Irancell wants to install a ... but if the distance to people is less
-%   than 100m it hurts them. now Irancell wants to maximize the signal
-%   power and minimize people's damage
+%   determine gender by height, weight & foot size
 
 
 %% clear everything
@@ -9,37 +7,49 @@ clc
 clear
 close all
 
-%% constants
-maxGen=1000;
-peoplesize = 30;
-popsize=50;
-ER=0.5;
-NN= round(ER * popsize);
-P = 0.3;
-K = 5;
 
+%% constants
+classesNo = 2;
+featuresNo = 3;
 
 %% trainig data 
-height = [6, 5.92, 5.58, 5.92, 5, 5.5, 5.42, 5.75];
-weight = [180, 190, 170, 165, 100, 150, 130, 150];
-footSize = [12, 11, 12, 10, 6, 8, 7, 9];
-gender = [0, 0, 0, 0, 1, 1, 1, 1]; % 0 = male, 1 = female
+fr(1,:) = [6, 5.92, 5.58, 5.92, 5, 5.5, 5.42, 5.75];
+fr(2,:) = [180, 190, 170, 165, 100, 150, 130, 150];
+fr(3,:) = [12, 11, 12, 10, 6, 8, 7, 9];
+gender = [2, 2, 2, 2, 1, 1, 1, 1]; % 2 = male, 1 = female
 
 
+%% seperate genders
+N = numel(gender);
+features = [[[],[],[]], [[],[],[]]]
 
-%% Optimization:
-for generation=1:maxGen
-    for i=1:popsize
-        fitness(i)=objfcn(people,chromosome(i));
+for i=1:N
+    for f=1:featuresNo
+        for c=1:classesNo
+            if gender(i)==c
+                features(c,f,end+1) = fr(f,i)
+            end
+        end
     end
-    [val,idx] = sort(fitness);
-    
+end
+        
+
+%% train
+for f=1:featuresNo
+    for c=1:classesNo
+        if gender(i)==1
+            means(c,f) = mean(features(c,f))
+            vars(c,f) = var(features(c,f))
+        end
+    end
+end
+
+%%
     
     % plot
-    disp(['best fitness=', num2str(val(end))]);
-    plot(generation,val(end),'k.');
-    hold on
-    pause(0.0000001)
+%     disp(['best fitness=', num2str(val(end))]);
+%     plot(generation,val(end),'k.');
+%     hold on
+%     pause(0.0000001)
     
-end
 
